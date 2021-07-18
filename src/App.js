8f33fed64e92e82c8fd85ca0b2795da7ddb9f742
king-handy <a.king.life@pm.me>
2021-07-18 19:27:08 -0400

    router begin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <nav>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/pizza'>Pizza</Link>
+        </li>
+      </nav>
+      <div className='App'>
+        {/* <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path ='/pizza' component={Pizza} />
+        </Switch> */}
+      </div>
+    </Router>
   );
 };
 export default App;
